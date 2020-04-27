aa437615972935a5914045e6ae85b43c059f2ca6
Mareks Bietags <bietagsmareks@gmail.com>
2020-04-27 10:21:47 +0300

    first commit

---
+++ android/app/src/main/kotlin/mareksbietags/fluttershare/MainActivity.kt
@@ -0,0 +1,12 @@
+package mareksbietags.fluttershare
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
